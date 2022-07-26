d74dd14eee09d2e97632b1d89a67f3e392a4f000
ibrohim8212 <iqudratov868@gmail.com>
2022-07-26 18:02:06 +0500

    Add sur
    Signed-off-by: ibrohim8212 <iqudratov868@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var intro = alert("Kimsiz?");
+
+var first_name = prompt("Ismingiz nima?", "Ibrohim");
+
+var last_name = prompt("Familiyangiz nima?", "Qudratov");
+
+var how_old = prompt("Yoshingiz nechida?", "15");
+
+var profesion = prompt("Caspingiz nima?", "Progromist");
+
+var home = prompt("Qaerdansiz?", "Toshkent");
+
+var confirm_info = confirm("Rahmat!")
+
+console.log(`informatcia:
+ ${first_name}
+ ${last_name}
+ ${how_old}
+ ${profesion}
+ ${home}
+ ${confirm_info}`);
